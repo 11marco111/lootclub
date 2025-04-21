70bb895859c0e34a068c64431b0caeeb129a6ada
Marco <32936667+11marco111@users.noreply.github.com>
2025-04-21 12:46:06 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,4 +15,4 @@ const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
 
-export { auth, googleProvider, facebookProvider };
+export { auth, googleProvider, facebookProvider, provider };
