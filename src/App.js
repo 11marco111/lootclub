7e18189419ac84f9bb362cd9c3503d1ff449d36f
11marco111 <marco.fortaleza@hotmail.it>
2025-04-21 16:11:46 +0200

    Aggiornamento file completo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Signup from './Signup';
-import Login from './Login';
-import Dashboard from './Dashboard';
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  );
-}
-
-export default App;
+// Placeholder for App.js
